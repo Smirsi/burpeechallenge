c764626c47a8762cb27c8125fbdadd73dd3d6b4f
Smirsi <89926925+Smirsi@users.noreply.github.com>
2024-06-30 07:18:38 +0200

    changed path

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from whatstk.data import whatsapp_urls
 st.set_page_config(layout="wide")
 st.title('Burpee- und Laufchallenge')
 
-df = df_from_whatsapp("path/to/_chat.txt")
+df = df_from_whatsapp("_chat.txt")
 st.dataframe(df)
