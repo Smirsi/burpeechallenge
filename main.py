35b20a20983eeb14432c6b97c38dd9ae5bc1fe96
Smirsi <89926925+Smirsi@users.noreply.github.com>
2025-11-09 19:38:10 +0100

    changed style

--- main.py
+++ main.py
@@ -279,6 +279,6 @@ for i in range(len(df['Punkte'])):
 
 st.divider()
 c1, c2 = st.columns(2)
-c1.markdown('Daten von 12.10.2025 21:27')
+c1.markdown('Daten von 09.11.2025 19:37')
 # if st.button('Daten updaten', type="primary", use_container_width=True):
 #    update_data_file(file_path)
